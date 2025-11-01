22b3dc33cfc5cc3314ddc96c87d0ce0503e2d305
tyang019 <tyang019@fiu.edu>
2025-11-01 18:38:15 -0400

    initialized new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import Home from "./Home"
 import AboutMe from"./AboutMe"
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import { useState } from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
-   const [loggenIn, setLoggedIn] = useState(false);
-  const login = () => setLoggedIn(true);
-  const logout = () => setLoggedIn(false);
   return (
     <div className='App'>
       <nav className='nav-bar'>
@@ -20,16 +14,6 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/about-me" element={<AboutMe />} />
       </Routes>
-        {
-        loggenIn ? (
-          <div>
-            <Home />
-            <LogoutButton logout={logout} />
-          </div>
-        ) : (
-            <LoginButton login={login} />
-        )
-      }
     </div>
   );
 }
--- src/LoginButton.js
+++
@@ -1,8 +0,0 @@
-function LoginButton(props){
-  return(
-    <button onClick={props.login}> 
-      Login
-    </button>
-  );
-};
-export default LoginButton;
